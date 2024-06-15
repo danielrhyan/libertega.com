d50b91b0b3a72ed60db74bdcc3c9788ea0f43c1a
danielrhyan <63158139+danielrhyan@users.noreply.github.com>
2024-06-15 09:10:21 -0600

    fixed navbar

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function () {
-  $("#navbar-placeholder").load("components/navbar-comp.html");
+  $("#navbar-placeholder").load("../components/navbar-comp.html");
 });
 $(function () {
-  $("#footer-placeholder").load("components/footer-comp.html");
+  $("#footer-placeholder").load("../components/footer-comp.html");
 });
